033c1ed163befbea071dd736bcef9d97203bd85b
eunssoo <eunssoo02@gmail.com>
2026-09-16 18:38:26 +0900

    Add files via upload

---
+++ day5/assignment/Programming01/Programming01.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Programming01 {
+
+    public static void main(String[] args) {
+
+        Circle c1 = new Circle(5);
+        ColoredCircle c2 = new ColoredCircle(10, "빨간색");
+
+        c1.show();
+        c2.show();
+    }
+}
